9ace4925e9ab0a5c400ec9eb17304c107ae8391a
eabartlett <eabartlett@berkeley.edu>
2014-10-08 15:53:30 -0700

    Some working stuff

--- proj2/search.py
+++ proj2/search.py
@@ -207,17 +207,15 @@ def getPredecessors(problem, extractPos=lambda x: x, generateState=lambda x: x):
                 predecessors[s].append((action, extractPos(s)))
     return predecessors
 
-def generateSuccessorState(predecessors={}, time=0):
+def generateSuccessorState(predecessors={}, time=0, initial=False):
     actions = [game.Directions.EAST,game.Directions.SOUTH,game.Directions.WEST,game.Directions.NORTH]
     t_actions = exactlyOne([logic.PropSymbolExpr(a, time-1) for a in actions])
-    if time <= 1:
-        return [exactlyOne([logic.PropSymbolExpr("P",pos[0],pos[1],time) for pos in predecessors.keys()])]+\
-            [logic.Expr(">>", logic.PropSymbolExpr("P",pos[0],pos[1],time), \
+    if time < 1:
+        return []
+    return [exactlyOne([logic.PropSymbolExpr("P",pos[0],pos[1],time) for pos in predecessors.keys()])] +\
+           [logic.Expr(">>", logic.PropSymbolExpr("P",pos[0],pos[1],time), \
             exactlyOne([logic.Expr("&", logic.PropSymbolExpr(a, time-1), logic.PropSymbolExpr("P",p[0],p[1],time-1))\
-            for (a, p) in preds])) for (pos, preds) in predecessors.items()] + list(t_actions)
-    return [logic.Expr(">>", logic.PropSymbolExpr("P",pos[0],pos[1],time), \
-            exactlyOne([logic.Expr("&", logic.PropSymbolExpr(a, time-1), logic.PropSymbolExpr("P",p[0],p[1],time-1))\
-            for (a, p) in preds])) for (pos, preds) in predecessors.items()] + list(t_actions) +\
+            for (a, p) in preds])) for (pos, preds) in predecessors.items()] + [t_actions] +\
            generateSuccessorState(predecessors,time-1)
 
 
