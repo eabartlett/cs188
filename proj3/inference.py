1dc673ec028910a89af21d87062f74ea715fc931
eabartlett <eabartlett@berkeley.edu>
2014-11-01 17:06:01 -0700

    Q1 working

--- proj3/inference.py
+++ proj3/inference.py
@@ -149,19 +149,17 @@ class ExactInference(InferenceModule):
         emissionModel = busters.getObservationDistribution(noisyDistance)
         pacmanPosition = gameState.getPacmanPosition()
 
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
-        # Replace this code with a correct observation update
-        # Be sure to handle the "jail" edge case where the ghost is eaten
-        # and noisyDistance is None
         allPossible = util.Counter()
-        for p in self.legalPositions:
-            trueDistance = util.manhattanDistance(p, pacmanPosition)
-            if emissionModel[trueDistance] > 0:
-                allPossible[p] = 1.0
 
-        "*** END YOUR CODE HERE ***"
+        if noisyDistance != None:
+            for p in self.legalPositions:
+                trueDistance = util.manhattanDistance(p, pacmanPosition)
+                if emissionModel[trueDistance] > 0:
+                    allPossible[p] = emissionModel[trueDistance] * self.beliefs[p]
+        else:
+            for p in self.legalPositions:
+                allPossible[p] = 0
+            allPossible[self.getJailPosition()] = 1
 
         allPossible.normalize()
         self.beliefs = allPossible
