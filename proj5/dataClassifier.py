03146a7e39781ffa999ade000e02dcd4e86e91e4
Justin MacMillin <Justin@Justins-MacBook-Pro.local>
2014-12-01 17:35:42 -0800

    so close Q3

--- proj5/dataClassifier.py
+++ proj5/dataClassifier.py
@@ -91,14 +91,19 @@ def enhancedFeatureExtractorDigit(datum):
     features =  basicFeatureExtractorDigit(datum)
     ROWS = "rows"
     ROWS2 = "rows2"
+    ROWS3 = "rows3"
     TOP_HEAVY = "top"
     BOTTOM_HEAVY = "bottom"
     TOP_HEAVY2 = "top2"
     BOTTOM_HEAVY2 = "bottom2"
+    TOP_HEAVY3 = "top3"
+    BOTTOM_HEAVY3 = "bottom3"
     LEFT_HEAVY = "left"
     RIGHT_HEAVY = "right"
     LEFT_HEAVY2 = "left2"
     RIGHT_HEAVY2 = "right2"
+    LEFT_HEAVY3 = "left3"
+    RIGHT_HEAVY3 = "right3"
     QUAD0 = "quad0"
     QUAD1 = "quad1"
     QUAD2 = "quad2"
@@ -106,16 +111,24 @@ def enhancedFeatureExtractorDigit(datum):
     NUMBER_WIDTH = "width"
     NUMBER_WHITE = "white"
     NUMBER_BLACK = "black"
+    NUMBER_BLACK2 = "black2"
+    NUMBER_BLACK3 = "black3"
     features[TOP_HEAVY], features[BOTTOM_HEAVY] = top_bottom(datum)
     features[LEFT_HEAVY], features[RIGHT_HEAVY] = left_right(datum)
     features[TOP_HEAVY2], features[BOTTOM_HEAVY2] = top_bottom(datum)
     features[LEFT_HEAVY2], features[RIGHT_HEAVY2] = left_right(datum)
+    features[TOP_HEAVY3], features[BOTTOM_HEAVY3] = top_bottom(datum)
+    features[LEFT_HEAVY3], features[RIGHT_HEAVY3] = left_right(datum)
     features[ROWS] = similar_color_rows(datum)
+    features[ROWS2] = similar_color_rows(datum)
+    features[ROWS3] = similar_color_rows(datum)
     # features[QUAD0], features[QUAD1], features[QUAD2], features[QUAD3] = quadrant_counts(datum)
     features[ROWS2] = similar_color_rows(datum)
     features[NUMBER_WIDTH] = number_width(datum)
     features[NUMBER_WHITE] = number_white(datum)
     features[NUMBER_BLACK] = number_black(datum)
+    features[NUMBER_BLACK2] = number_black(datum)
+    features[NUMBER_BLACK3] = number_black(datum)
 
     return features
 
